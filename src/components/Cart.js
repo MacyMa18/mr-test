074393abc681b044b174507cb3f35b674061976b
MacyMa18 <macyma18@gmail.com>
2019-10-05 15:23:30 +1300

    feat: add Cart & ProductInfo components

---
+++ src/components/Cart.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Cart.scss';
+
+class Cart extends Component {
+  render() {
+    return <div className="section-wrapper section-cart"></div>;
+  }
+}
+
+export default Cart;
---
+++ src/components/ProductInfo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './ProductInfo.scss';
+
+class ProductInfo extends Component {
+  render() {
+    return <div className="section-product-info"></div>;
+  }
+}
+
+export default ProductInfo;
