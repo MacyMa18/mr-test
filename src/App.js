37019c9b9e4c61fdefc396550aaefdfb76df4530
MacyMa18 <macyma18@gmail.com>
2019-10-05 15:10:28 +1300

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Product from './components/Product';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Product />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Cart from './Cart';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className="header-section">
+      <Cart />
+    </div>
+  );
+};
+
+export default Header;
