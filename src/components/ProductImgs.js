bdeceffc25f0b03472309b8564cfce93ecda668e
MacyMa18 <macyma18@gmail.com>
2019-10-05 15:19:09 +1300

    feat: add ProductImgs component

---
+++ src/components/ProductImgs.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './ProductImgs.scss';
+
+class ProductImgs extends Component {
+  render() {
+    return (
+      <div className="section-product-imgs">
+        <img
+          src="https://drive.google.com/file/d/0B8KYnbdnrRGXSXVoMzdqRWhCTXc/view?usp=sharing"
+          alt="product-image"
+        />
+      </div>
+    );
+  }
+}
+
+export default ProductImgs;
