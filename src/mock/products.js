bb2270284582f4308ca72c907f4a21aebb12005d
MacyMa18 <macyma18@gmail.com>
2019-10-07 17:05:12 +1300

    fix: change the demo image's cdn url

--- src/mock/products.js
+++ src/mock/products.js
@@ -6,7 +6,7 @@ export default [
     unit: '$',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    images: ['https://cdn.jsdelivr.net/gh/HY-BOT/CDN@master/classic-tee.jpg'],
+    images: ['https://assets.sapi.work/temp/classic-tee.jpg'],
     sku_size: [
       {
         sku_id: 'size_s',
